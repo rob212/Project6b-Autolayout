136ddd14b222e7752afd00e9b1af2157a76a8127
Robert McBryde <robert.mcbryde@kotikan.com>
2015-10-27 18:09:38 +0000

    Initial Commit

---
+++ Project6b-AutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project6b-AutoLayout
+//
+//  Created by Robert McBryde on 27/10/2015.
+//  Copyright © 2015 Robert McBryde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Project6b-AutoLayoutTests/Project6b_AutoLayoutTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project6b_AutoLayoutTests.swift
+//  Project6b-AutoLayoutTests
+//
+//  Created by Robert McBryde on 27/10/2015.
+//  Copyright © 2015 Robert McBryde. All rights reserved.
+//
+
+import XCTest
+@testable import Project6b_AutoLayout
+
+class Project6b_AutoLayoutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
